e06924c7cbb8e585aa65401b0300c2e08e34ae55
qweewq <seongkh15@naver.com>
2024-01-26 19:50:44 +0900

    delete todo

--- front/todo.js
+++ front/todo.js
@@ -3,22 +3,41 @@ document.addEventListener('DOMContentLoaded', () => {
   const addBtn = document.querySelector('.addBtn'); 
   const todoLists = document.querySelector('.todo-list')
   let inputTodo = document.querySelector('.inputTodo');
-  let result = document.querySelector('result');
-  
+
+  const delAllBtn = document.querySelector('.delAllBtn');
+
+  // 투두 리스트 추가
   addBtn.addEventListener('click', (e) => {
     const item = document.createElement('div');
 
     const text = document.createElement('span');
+
+    const delBtn = document.createElement('button');
+    delBtn.textContent = '삭제';
+    
     
     if(inputTodo.value != false) {
       text.textContent = inputTodo.value;
 
       item.appendChild(text);
       todoLists.append(item);
+      item.append(delBtn);
       
       inputTodo.value = '';
       inputTodo.focus();
     }
+
+    // 투두리스트 하나 삭제
+    delBtn.addEventListener('click', (e) => {
+        const item = e.target.parentNode;
+        todoLists.removeChild(item);
+    })
   })
 
+  // 전체 삭제
+  delAllBtn.addEventListener('click', (e) => {
+    while(todoLists.childNodes.length > 2) {
+      todoLists.removeChild(todoLists.lastChild);
+    }
+  })
 })
