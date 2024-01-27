b2de41cb4663da3cd1023448467577653d2109d8
qweewq <seongkh15@naver.com>
2024-01-27 17:45:12 +0900

    rest - post

--- back/src/main/java/com/todolist/todolist/rest/TodoListRestController.java
+++ back/src/main/java/com/todolist/todolist/rest/TodoListRestController.java
@@ -2,9 +2,9 @@ package com.todolist.todolist.rest;
 
 import com.todolist.todolist.dao.TodoListDAO;
 import com.todolist.todolist.entity.TodoLists;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.todolist.todolist.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -12,14 +12,28 @@ import java.util.List;
 @RequestMapping("/api")
 public class TodoListRestController {
 
-    private TodoListDAO todoListDAO;
+    private TodoService todoService;
 
-    public TodoListRestController(TodoListDAO theTodoDAO) {
-        todoListDAO = theTodoDAO;
+    @Autowired
+    public TodoListRestController(TodoService theTodoService) {
+        todoService = theTodoService;
     }
+
+    // 전체 조회
 
     @GetMapping("/lists")
     public List<TodoLists> findAll() {
-        return todoListDAO.findAll();
+        return todoService.findAll();
     }
+
+    // 게시글 작성
+    @PostMapping("/lists")
+    public TodoLists addTodo(@RequestBody TodoLists theTodo) {
+        theTodo.setId(0);
+
+        TodoLists dbTodo = todoService.save(theTodo);
+
+        return dbTodo;
+    }
+
 }
